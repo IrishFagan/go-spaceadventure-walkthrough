0c03a4e82795322130d828b497e40d31cf314596
Irish Fagan <IrishFagan@live.com>
2019-11-04 10:23:29 -0800

    Changed hanldeUserResponse to responseToPrompt

--- main.go
+++ main.go
@@ -10,18 +10,18 @@ func main() {
 
 func printIntroduction(greeting string, intro string) {
 	fmt.Println(greeting,intro)
-	name := handleUserResponse("What is your name?")
+	name := responseToPrompt("What is your name?")
 	fmt.Printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Siri.\n", name)
 }
 
 func travel() {
 	var choice string
 	for choice != "Y" && choice != "N" {
-		choice = handleUserResponse("Shall I randomly choose a planet for you to visit? (Y or N)")
+		choice = responseToPrompt("Shall I randomly choose a planet for you to visit? (Y or N)")
 		if choice == "Y" {
 			handlePlanetChoice("Traveling to Jupiter...","","Arrived at Jupiter. The large red spot appears ominous.")
 		} else if choice == "N" {
-			planetName := handleUserResponse("Name the planet you would like to vist!")
+			planetName := responseToPrompt("Name the planet you would like to vist!")
 			handlePlanetChoice("Traveling to", planetName,"Arrived at Neptune. A very cold planet, furthest from the sun.")
 		} else {
 			fmt.Println("Sorry, I didn't get that.")
@@ -29,7 +29,7 @@ func travel() {
 	}
 }
 
-func handleUserResponse(prompt string) string {
+func responseToPrompt(prompt string) string {
 	var response string
 	fmt.Println(prompt)
 	fmt.Scan(&response)
