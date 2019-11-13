d7ab7ecceaa3c67b08979c3000a0c02a323fa677
Irish Fagan <IrishFagan@live.com>
2019-11-13 14:38:42 -0800

    Fixed handling of name input. Added functionality to show how many planets there  in the solar system

--- cmd/spaceadventure/main.go
+++ cmd/spaceadventure/main.go
@@ -3,5 +3,11 @@ package main
 import "github.com/IrishFagan/go-spaceadventure-walkthrough/internal/spaceadventure"
 
 func main() {
-	spaceadventure.Start(spaceadventure.PlanetarySystem{"Solar System"})
+	spaceadventure.Start(
+		spaceadventure.PlanetarySystem{
+			Name: "Solar System", Planets: []spaceadventure.Planet{
+				spaceadventure.Planet{"Earth", "Nice planet"},
+			},
+		},
+	)
 }
