818a39133ea3e9430a91be1f58591897e72ab526
Irish Fagan <IrishFagan@live.com>
2019-11-20 16:07:40 -0800

    added mockPlanets function to return planets and their descripts!

--- cmd/spaceadventure/main.go
+++ cmd/spaceadventure/main.go
@@ -5,9 +5,22 @@ import "github.com/IrishFagan/go-spaceadventure-walkthrough/internal/spaceadvent
 func main() {
 	spaceadventure.Start(
 		spaceadventure.PlanetarySystem{
-			Name: "Solar System", Planets: []spaceadventure.Planet{
-				spaceadventure.Planet{"Earth", "Nice planet"}, 
-			},
+			Name: "Solar System", Planets: mockPlanets(),
 		},
 	)
+}
+
+func mockPlanets() []spaceadventure.Planet {
+	return []spaceadventure.Planet{
+		spaceadventure.Planet{"Geonosis","Rocky n' stuff"},
+		spaceadventure.Planet{"Kamnio","Clones n stuff r here"},
+		spaceadventure.Planet{"Utapau","Sinkholes n stufffff and deserts"},
+		spaceadventure.Planet{"Scarif","Blown up, dont visit"},
+		spaceadventure.Planet{"nur","OCEAN PLANET W/ VOLCANOES"},
+		spaceadventure.Planet{"Dog","Woof"},
+		spaceadventure.Planet{"Kessel","spice"},
+		spaceadventure.Planet{"Dathomire","Wet spice + witches"},
+		spaceadventure.Planet{"Coruscant","tech n stuff"},
+		spaceadventure.Planet{"Austin world","trash"},
+	}
 }
